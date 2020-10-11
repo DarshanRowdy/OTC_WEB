ce0017ab6419be9226401a54e8a3755d8c9a0729
Darshan Rathod <darshanrathod.info@gmail.com>
2020-10-12 00:51:12 +0530

    remove app and resource

--- resources/js/middlewares/auth.js
+++
@@ -1,8 +0,0 @@
-export default function auth({ next, router }) {
-    let userObj = JSON.parse(localStorage.getItem('userObj'));
-    if(userObj && userObj.auth_token){
-        return next();
-    } else {
-        return next('/login');
-    }
-}
