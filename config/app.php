f87117807962af20f33495ae04c4c89b49be3ba4
Darshan Rathod <darshanrathod.info@gmail.com>
2020-10-25 02:50:52 +0530

    url change for app_url in config

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://trade.otccapital.in'),
 
     'asset_url' => env('ASSET_URL', null),
     'default_limit' => 5,
